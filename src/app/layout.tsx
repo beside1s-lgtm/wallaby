028381656aa59393975eeec040363369ec483397
AI마스터 <aimaster@kshcm.net>
2025-10-29 05:12:56 +0000

    지금 앱을 웹에서 열거나 폰에 다운받으면 파이어베이스 아이콘이 나오는데 이걸 이 앱의 고유 이미지로 나오게 하고 싶어. 도와줄 수

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
